966e3006e40cad5979880d94212a5436a8c8ad95
yashishvin <56137245+yashishvin@users.noreply.github.com>
2021-02-26 22:53:03 -0600

    Update ProfileView.js

--- ProfileView.js
+++ ProfileView.js
@@ -12,16 +12,25 @@ class ProfileView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "",
-      password:"",
-      PhoneNumber:"",
-      Address:""
+      firstName: "",
+      lastName: "",
+      goalDailyCalories: 0.0,
+      goalDailyProtein: 0.0,
+      goalDailyCarbohydrates: 0.0,
+      goalDailyFat: 0.0,
+      goalDailyActivity: 0.0
     }
   }
 
-  
-  //The fetch call to get the information about the user goes here
-  /*componentDidMount() {
+  /**
+   * Fetch the data from the API after mounting; this may take a few seconds.
+   * Once the data is fetched, it is stored into the state and then displayed
+   * onto the fields.
+   * 
+   * This GET request requires us to specify our username and x-access-token,
+   * both of which we inherit through props.
+   */
+  componentDidMount() {
     console.log("here the user name in the component is"+this.props.username)
     fetch('https://mysqlcs639.cs.wisc.edu/users/' + this.props.username, {
       method: 'GET',
@@ -40,7 +49,58 @@ class ProfileView extends React.Component {
         });
       });
   }
-*/
+
+  /**
+   * Make a PUT request to save all the entered information onto the server.
+   * Note, we must check if what the user entered is a number. Once the state
+   * is guarnteed to be set, we call the fetch function.
+   * 
+   * This PUT request requires us to specify our username and x-access-token,
+   * both of which we inherit through props. Additionally, we are sending a
+   * JSON body, so we need to specify Content-Type: application/json
+   */
+  handleSaveProfile() {
+    this.setState({
+      goalDailyCalories: parseFloat(this.state.goalDailyCalories),
+      goalDailyProtein: parseFloat(this.state.goalDailyProtein),
+      goalDailyCarbohydrates: parseFloat(this.state.goalDailyCarbohydrates),
+      goalDailyFat: parseFloat(this.state.goalDailyFat),
+      goalDailyActivity: parseFloat(this.state.goalDailyActivity)
+    }, () => fetch('https://mysqlcs639.cs.wisc.edu/users/' + this.props.username, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-access-token': this.props.token
+      },
+      body: JSON.stringify({
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        goalDailyCalories: this.state.goalDailyCalories,
+        goalDailyProtein: this.state.goalDailyProtein,
+        goalDailyCarbohydrates: this.state.goalDailyCarbohydrates,
+        goalDailyFat: this.state.goalDailyFat,
+        goalDailyActivity: this.state.goalDailyActivity
+      })
+    })
+      .then(res => res.json())
+      .then(res => {
+        alert("Your profile has been updated!");
+      })
+      .catch(err => {
+        alert("Something went wrong! Verify you have filled out the fields correctly.");
+      }));
+
+  }
+
+  backToLogin() {
+    this.props.revokeAccessToken();
+  }
+
+  /**
+   * Displays and collects the profile information.
+   * 
+   * The styling could definitely be cleaned up; should be consistent!
+   */
   render() {
     return (
       <ScrollView style={styles.mainContainer} contentContainerStyle={{ flexGrow: 11, justifyContent: 'center', alignItems: "center" }}>
@@ -57,56 +117,96 @@ class ProfileView extends React.Component {
         <Text style={{ textAlignVertical: "center", fontWeight: "700", fontSize: 20 }}>Personal Information</Text>
         <View style={styles.spaceSmall}></View>
         <View>
-          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>User Name</Text>
+          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>First Name</Text>
         </View>
         <TextInput style={styles.input}
           underlineColorAndroid="transparent"
-          placeholder={this.props.username}
+          placeholder="Bucky"
           placeholderTextColor="#d9bebd"
-          onChangeText={(userName) => this.setState({ userName: userName})}
-          value={this.state.userName}
+          onChangeText={(firstName) => this.setState({ firstName: firstName })}
+          value={this.state.firstName}
           autoCapitalize="none" />
         <View style={styles.spaceSmall}></View>
 
         <View>
-          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Password</Text>
+          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Last Name</Text>
         </View>
         <TextInput style={styles.input}
           underlineColorAndroid="transparent"
           placeholder="Badger"
           placeholderTextColor="#d9bebd"
-          onChangeText={(password) => this.setState({ password:password })}
-          value={this.state.password}
+          onChangeText={(lastName) => this.setState({ lastName: lastName })}
+          value={this.state.lastName}
           autoCapitalize="none" />
         <View style={styles.spaceSmall}></View>
+
+        <Text style={{ textAlignVertical: "center", fontWeight: "700", fontSize: 20 }}>Fitness Goals</Text>
         <View style={styles.spaceSmall}></View>
         <View>
-          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Phone Number</Text>
+          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Daily Calories (kcal)</Text>
         </View>
         <TextInput style={styles.input}
           underlineColorAndroid="transparent"
-          placeholder="2200213213"
+          placeholder="2200"
           placeholderTextColor="#d9bebd"
-          onChangeText={(PhoneNumber) => this.setState({ PhoneNumber:PhoneNumber})}
-          value={this.state.PhoneNumber + ""}
+          onChangeText={(goalDailyCalories) => this.setState({ goalDailyCalories: goalDailyCalories })}
+          value={this.state.goalDailyCalories + ""}
           autoCapitalize="none" />
         <View style={styles.spaceSmall}></View>
         <View>
-          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Address</Text>
+          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Daily Protein (grams)</Text>
         </View>
         <TextInput style={styles.input}
           underlineColorAndroid="transparent"
-          placeholder="Nfrances street"
+          placeholder="52"
           placeholderTextColor="#d9bebd"
-          onChangeText={(Address) => this.setState({ Address: Address })}
-          value={this.state.Address}
+          onChangeText={(goalDailyProtein) => this.setState({ goalDailyProtein: goalDailyProtein })}
+          value={this.state.goalDailyProtein + ""}
           autoCapitalize="none" />
+        <View style={styles.spaceSmall}></View>
+        <View>
+          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Daily Carbs (grams)</Text>
+        </View>
+        <TextInput style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder="130"
+          placeholderTextColor="#d9bebd"
+          onChangeText={(goalDailyCarbohydrates) => this.setState({ goalDailyCarbohydrates: goalDailyCarbohydrates })}
+          value={this.state.goalDailyCarbohydrates + ""}
+          autoCapitalize="none" />
+        <View style={styles.spaceSmall}></View>
+        <View>
+          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Daily Fat (grams)</Text>
+        </View>
+        <TextInput style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder="35"
+          placeholderTextColor="#d9bebd"
+          onChangeText={(goalDailyFat) => this.setState({ goalDailyFat: goalDailyFat })}
+          value={this.state.goalDailyFat + ""}
+          autoCapitalize="none" />
+        <View style={styles.spaceSmall}></View>
+        <View>
+          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Daily Activity (mins)</Text>
+        </View>
+        <TextInput style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder="60"
+          placeholderTextColor="#d9bebd"
+          onChangeText={(goalDailyActivity) => this.setState({ goalDailyActivity: goalDailyActivity })}
+          value={this.state.goalDailyActivity + ""}
+          autoCapitalize="none" />
+        <View style={styles.spaceSmall}></View>
+
+        <View style={styles.space} />
 
         <Text style={{ fontWeight: "700", fontSize: 20 }}>Looks good! All set?</Text>
         <View style={styles.spaceSmall} />
 
         <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-          <Button color="#942a21" style={styles.buttonInline} title="Save Profile"  />
+          <Button color="#942a21" style={styles.buttonInline} title="Save Profile" onPress={() => this.handleSaveProfile()} />
+          <View style={styles.spaceHorizontal} />
+          <Button color="#a1635f" style={styles.buttonInline} title="Exit" onPress={() => this.backToLogin()} />
         </View>
         <View style={styles.space} />
       </ScrollView>
